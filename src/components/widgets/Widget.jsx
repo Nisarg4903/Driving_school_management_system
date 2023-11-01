f5a4b6fe138a2975b0327b663a88db80c4f1a83b
Nisarg4903 <patelrumi4903@gmail.com>
2023-11-01 14:35:39 +0300

    Just more changes

--- src/components/widgets/Widget.jsx
+++ src/components/widgets/Widget.jsx
@@ -1,21 +1,68 @@
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
-const Widget = () => {
+const Widget = ({ type }) => {
+  let data;
+  //temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: <PersonOutlineOutlinedIcon className="icon" />,
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "See all orders",
+        icon: <ShoppingCartOutlinedIcon className="icon" />,
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: <MonetizationOnOutlinedIcon className="icon" />,
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: <AccountBalanceWalletOutlinedIcon className="icon" />,
+      };
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="widget">
       <div className="left">
-        <span className="title">USERS</span>
-        <span className="counter">1234</span>
-        <span className="link">See all users</span>
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
       </div>
       <div className="right">
-        <div className="percentage">
+        <div className="percentage positive">
           <KeyboardArrowUpIcon />
-          20%
+          {diff}%
         </div>
-        <PersonOutlineOutlinedIcon className="icon"/>
+        {data.icon}
       </div>
     </div>
   );
