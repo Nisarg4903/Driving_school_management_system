816f08b6d2433364371c3c0696bd678a2673df1f
Nisarg4903 <patelrumi4903@gmail.com>
2023-11-01 11:54:57 +0300

    First commit of the project

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">lamadmin</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonOutlineOutlinedIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <Inventory2OutlinedIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <p className="title">USEFULL</p>
+          <li>
+            <BarChartOutlinedIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/widgets/Widget.jsx
@@ -0,0 +1,24 @@
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+
+const Widget = () => {
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">USERS</span>
+        <span className="counter">1234</span>
+        <span className="link">See all users</span>
+      </div>
+      <div className="right">
+        <div className="percentage">
+          <KeyboardArrowUpIcon />
+          20%
+        </div>
+        <PersonOutlineOutlinedIcon className="icon"/>
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
